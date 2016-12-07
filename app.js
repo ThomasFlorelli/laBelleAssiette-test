8be93a9492d41fb5adce67313248d33fff0a6dcd
danielprudhomme <dprudhomme741@gmail.com>
2016-12-07 09:27:50 +0100

    First step: node express app launching angularjs app

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express'),
+app = express(),
+util = require('util');
+
+app.path = path = require('path');
+app.jsHandler = jsHandler = '';
+
+// app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
+// app.use(express.static(path.join(__dirname, '/public')));
+// var server = app.listen(7001, function () {
+// 	console.log('Server is running on  ' + server.address().address + ':' + server.address().port);
+// });
+
+// Common app config
+app.configure(function() {
+	app.use(express.urlencoded({
+		limit: '10mb'
+	}));
+
+	app.use(express.json());
+
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('lbajobs'));
+
+	app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
+	app.use(express.static(path.join(__dirname, '/public')));
+
+	/*
+		Starting app
+	*/
+	var server = app.listen(7001, function () {
+		console.log('Server is running on  ' + server.address().address + ':' + server.address().port);
+	});
+});
---
+++ public/components/main/main.component.js
@@ -0,0 +1,11 @@
+(function(){
+  angular.module('laBelleAssietteApp')
+    .component('main', {
+      templateUrl: '/components/main/main.component.html',
+      component: MainComponent
+    });
+    
+  function MainComponent() {
+    
+  }
+})();
